53533a7698c4d867e5f705ce559cf2c694572e4d
임정빈 <mhosk25@gmail.com>
2023-02-18 23:06:20 +0900

    git commit

---
+++ 입출력.py
@@ -0,0 +1,18 @@
+# #1000
+# a, b = input().split()
+#
+# print(int(a) + int(b))
+
+# #10926
+# id = input()
+#
+# print(id + "??!")
+
+# #18108
+# year = int(input())
+# print(year - 543)
+
+#3003
+user_input = input()
+numbers = [int(num) for num in user_input.split()]
+print(numbers)
