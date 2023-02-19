c7bc8b3cc5793df002f00a15a5364c0755d8eda7
임정빈 <mhosk25@gmail.com>
2023-02-19 18:02:57 +0900

    git commit

--- 입출력.py
+++ 입출력.py
@@ -12,18 +12,47 @@
 # year = int(input())
 # print(year - 543)
 
-#3003
-user_input = input()
-numbers = [int(num) for num in user_input.split()]
+# #3003
+# user_input = input()
+# numbers = [int(num) for num in user_input.split()]
+#
+# piece = [1, 1, 2, 2, 2, 8]
+# n = len(piece)
+# modifing_piece = [0] * n
+#
+# for i in range(6):
+#     if numbers[i] == piece[i]:
+#         modifing_piece[i] = 0
+#     else:
+#         modifing_piece[i] = piece[i] - numbers[i]
+#
+# print(' '.join(str(s) for s in modifing_piece))
 
-piece = [1, 1, 2, 2, 2, 8]
-n = len(piece)
-modifing_piece = [0] * n
+# #3003 - using map function
+# chess = [1, 1, 2, 2, 2, 8]
+#
+# a = list(map(int, input().split()))
+#
+# for i in range(len(a)):
+#     print(chess[i] - a[i], end=' ')
 
-for i in range(6):
-    if numbers[i] == piece[i]:
-        modifing_piece[i] = 0
-    else:
-        modifing_piece[i] = piece[i] - numbers[i]
+# #2588 - 나머지를 구하는 %을 이용하여 값을 바로 출력하는 방법
+# num1 = int(input())
+# num2 = int(input())
+#
+# print(num1 * (num2%10))
+# print(num1 * ((num2%100)//10))
+# print(num1 * (num2//100))
+# print(num1 * num2)
 
-print(' '.join(str(s) for s in modifing_piece))
+#2588 - 결과값을 list 변수에 담고, 출력하는 방법
+num1 = int(input())
+num2 = list(map(int, input()))
+
+result = []
+
+for i in range(len(num2), 0, -1):
+    result.append(num1 * num2[i - 1])
+
+print(result[0], result[1], result[2], sep='\n')
+print(result[0] + (result[1]*10) + result[2] * 100)
