cfc4efcd47b68ab507263a6f31f4aaa6f86ed721
임정빈 <mhosk25@gmail.com>
2023-02-20 12:10:41 +0900

    git commit

--- 조건문.py
+++ 조건문.py
@@ -5,3 +5,30 @@
 #
 # else: print(0)
 
+# #14681
+# x = int(input())
+# y = int(input())
+#
+# if x > 0 and y > 0:
+#     print(1)
+# elif x < 0 and y > 0:
+#     print(2)
+# elif x < 0 and y < 0:
+#     print(3)
+# else: print(4)
+
+#2884
+(hour, min) = tuple(map(int, input().split()))
+
+if hour != 0:
+    if min < 45:
+        (hour, min) = (hour - 1, 15 + min)
+    else:
+        (hour, min) = (hour, min - 45)
+else:
+    if min < 45:
+        (hour, min) = (23, 15 + min)
+    else:
+        (hour, min) = (hour, min - 45)
+
+print(hour, min)
