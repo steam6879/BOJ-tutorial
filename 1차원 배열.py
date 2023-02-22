78d08c8d54170ae74409c630ea2fc93b600cb4b0
STEAM <mhosk25@gmail.com>
2023-02-22 15:42:50 +0900

    git commit

--- 1차원 배열.py
+++ 1차원 배열.py
@@ -1,10 +1,18 @@
-#10871
-n, x = map(int, input().split())
-a = list(map(int, input().split()))
-result = []
+# #10871
+# n, x = map(int, input().split())
+# a = list(map(int, input().split()))
+# result = []
+#
+# for i in range(n):
+#     if a[i] < x:
+#         result.append(a[i])
+#
+# print(' '.join(map(str, result)))
 
-for i in range(n):
-    if a[i] < x:
-        result.append(a[i])
+#2562
+a = []
+for i in range(9):
+    a.append(int(input()))
 
-print(' '.join(map(str, result)))
+print(max(a))
+print(a.index(max(a)) + 1)
