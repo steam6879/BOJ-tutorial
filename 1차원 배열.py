0c9fb518ba56d1eb179fdec0216767b367a4c880
STEAM <mhosk25@gmail.com>
2023-02-22 16:30:03 +0900

    git commit

--- 1차원 배열.py
+++ 1차원 배열.py
@@ -1,4 +1,4 @@
-# #10871
+# # 10871
 # n, x = map(int, input().split())
 # a = list(map(int, input().split()))
 # result = []
@@ -9,10 +9,21 @@
 #
 # print(' '.join(map(str, result)))
 
-#2562
-a = []
-for i in range(9):
-    a.append(int(input()))
+# # 2562
+# a = []
+# for i in range(9):
+#     a.append(int(input()))
+#
+# print(max(a))
+# print(a.index(max(a)) + 1)
 
-print(max(a))
-print(a.index(max(a)) + 1)
+# 10810
+basket_num, n = map(int, input().split())
+basket = [0] * basket_num
+
+for i in range(n):
+    start_num, end_num, key = map(int, input().split())
+    for j in range(start_num - 1, end_num):
+        basket[j] = key
+
+print(' '.join(map(str, basket)))
